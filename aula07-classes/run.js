0727a8b076a83c7758dd0310843d76dcfd212828
shadowdeveloper <ro_web1@hotmail.com>
2017-07-30 10:42:31 -0300

    classes, erancas e sobrescrita de metodos

---
+++ aula07-classes/run.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var cavalo_1 = require("./cavalo");
+var animal_1 = require("./animal");
+var a = new animal_1.Animal("Rafael");
+a.move(10);
+var c = new cavalo_1.Cavalo("Aline");
+c.move(150);
+//# sourceMappingURL=run.js.map
---
+++ aula07-classes/run.ts
@@ -0,0 +1,8 @@
+import { Cavalo } from './cavalo';
+import { Animal } from "./animal";
+
+let a = new Animal("Rafael");
+a.move(10);
+
+let c = new Cavalo("Aline");
+c.move(150);
