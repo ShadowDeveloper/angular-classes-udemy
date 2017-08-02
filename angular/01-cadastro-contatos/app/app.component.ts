df826d584e7725622022c4928d3a0974454b6700
Rodrigo Bruno de Souza <rodrigo.souza@norber.com.br>
2017-08-02 18:57:49 -0300

    aplication structure and files

---
+++ angular/01-cadastro-contatos/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: 'my-app', // <my-app></my-app>
+  template:`
+    <h1>Angular App</h1>
+    <button class="btn btn-success">Boostrap Button</button>
+  `
+})
+
+export class AppComponent {
+  
+}
+
