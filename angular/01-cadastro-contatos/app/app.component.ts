0d50ff6798656b5aaada34b4b96f8dc522c6ab4e
shadowdeveloper <ro_web1@hotmail.com>
2017-08-08 21:38:15 -0300

    botao de novo contato

--- angular/01-cadastro-contatos/app/app.component.ts
+++ angular/01-cadastro-contatos/app/app.component.ts
@@ -1,11 +1,9 @@
 import { Component } from "@angular/core";
 
 @Component({
+  moduleId:module.id,
   selector: 'my-app', // <my-app></my-app>
-  template:`
-    <h1>Angular App</h1>
-    <router-outlet></router-outlet>
-  `
+  templateUrl:'app.component.html'
 })
 
 export class AppComponent {
